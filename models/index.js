0e05f6934197bdcf77a74a93869eabbff6b5bd86
Jas James <jasdjames@gmail.com>
2021-03-10 14:25:01 -0600

    added base information in the models index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,41 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+
+
+
+
+
+
+});
 // Categories have many Products
+Category.belongsToMany (Product, {
+
+
+
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+through: {
+  model:ProductTag,
+  unique:false
+},
+
+// as:''
+
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model:ProductTag,
+    unique:false
+  },
+
+})
 
 module.exports = {
   Product,
